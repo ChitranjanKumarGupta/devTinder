c919b83bbf38e527d16456c06749346a618b8eda
Chitranjan <chitranjangupta51@gmail.com>
2024-10-23 14:27:37 -0400

    explore the http method

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@ const express = require('../node_modules/express');
 
 const app = express();
 
-app.use((req, res) => {
+//this will only handle GET call 
+app.get("/user",(req,res) => {
+    res.send({fistname: "Chitranjan", lastname:"Gupta"});
+})
+
+
+// this will match all the HTTP Method API call 
+app.use("/test",(req, res) => {
     res.send("Hello from the server")
 });
 app.listen(3000,() => {
     console.log("Server is successfully listing on port 3000.....");
     
-});
+});  
